e72ad701f1fddf969e29da97f469418e05476bcc
Chantal Sorias <chantal_sorias@hotmail.com>
2025-01-02 16:34:29 -0500

    random photo generator functionality

--- RandomPhotoGenerator/ViewController.swift
+++ RandomPhotoGenerator/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colours: [UIColor] = [
+        .systemRed,
+        .systemOrange,
+        .systemYellow,
+        .systemGreen,
+        .systemBlue,
+        .systemPurple
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        getRandomPhoto()
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 60)
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+        view.backgroundColor = colours.randomElement()
+    }
+    
+    func getRandomPhoto() {
+        let urlString = "https://picsum.photos/600/600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 
 }
 
